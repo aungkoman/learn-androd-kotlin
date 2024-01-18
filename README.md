794f26735ece2fe92993396668fcce18c11af780
Aung Ko Man <aungkoman@gmail.com>
2024-01-18 21:22:44 +0630

    full width image

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Learn Android Repo (Kotlin Version)
 
+## ဒီမှာ လေ့လာ
+
+https://developer.android.com/courses/pathways/android-basics-compose-unit-1-pathway-3
+
+
+ဓာတ်ပုံ အပြည့်ပေါ်စေဖို့
+
+```kotlin
+Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            // Ensure the image takes the entire screen
+            modifier = Modifier.fillMaxSize().alpha(0.5F)
+        )
+```
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,10 +49,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun BirthdayCard() {
-    Row {
-        Text("text one",)
-        Text("text two")
+fun BirthdayCard(from: String, to: String, modifier: Modifier=Modifier ) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = "Happy Birthday $to",
+            fontSize = 100.sp,
+            lineHeight = 106.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "By $from",
+            fontSize = 36.sp,
+            modifier = Modifier.padding(8.dp).align(alignment = Alignment.End)
+        )
     }
 }
 
@@ -53,8 +72,22 @@ fun BirthdayCard() {
 fun GreetingImage() {
     val image = painterResource(id = R.drawable.background_image)
     Box {
-        Image(painter = image, contentDescription = null)
-        BirthdayCard()
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            // Ensure the image takes the entire screen
+            modifier = Modifier
+                .fillMaxSize()
+                .alpha(0.5F)
+        )
+        BirthdayCard(
+            from = "KM",
+            to = "MSD",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
 
