587a52830cac43bbf5d0ba88212047b070e945b2
Aung Ko Man <aungkoman@gmail.com>
2024-01-18 21:26:20 +0630

    debuging tips

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ Image(
             modifier = Modifier.fillMaxSize().alpha(0.5F)
         )
 ```
+
+ကိုယ့်ရဲ့ Compose Widget က ဘယ်လောက် နေရာယူနေလဲ သိချင်ရင် modifier မှာ Background Color ထည့်ကြည့်လိုက်။
+
+```kotlin
+modifier = Modifier
+        .fillMaxSize()
+    .padding(8.dp)
+    .background(color = Color.Blue)
+```
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -87,6 +89,7 @@ fun GreetingImage() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
+                .background(color = Color.Blue)
         )
     }
 }
